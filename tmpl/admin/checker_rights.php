7c641d562c21409d5ffab8e55666d90a95683fea
Webtopay <Webtopay@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-05 12:56:57 +0000

    Added v5.0 version of Evp-Events plugin
    git-svn-id: https://plugins.svn.wordpress.org/evp-events/trunk@692194 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tmpl/admin/checker_rights.php
@@ -0,0 +1,7 @@
+<h2><?php _e('Pair your mobile device', 'evp-event')?></h2>
+<p><?php _e('Pair your mobile device with particular event to get ability validate tickets. Just read this QR code with your device 
+		and follow the link.', 'evp-event') ?>
+</p>
+<p><?php _e('Your mobile device must accept cookies!', 'evp-event') ?></p>
+
+<img style='margin:40px' src='data:image/jpeg;base64,<?php echo $raw_png ?>' />
