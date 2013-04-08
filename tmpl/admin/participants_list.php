24849110ac1ce7610664e4ec58591d247937bd98
Webtopay <Webtopay@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-08 09:43:20 +0000

    Deleted files
    git-svn-id: https://plugins.svn.wordpress.org/evp-events/trunk@693557 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tmpl/admin/participants_list.php
+++
@@ -1,7 +0,0 @@
-<h2><?php _e("Orders", "evp-event") ?></h2>
-<!--<?php if(isset($_GET['success'])): ?>
-	<div id="message" class="updated below-h2"><p><?php _e("Order successfully saved", "evp-event") ?></p></div>
-<? endif ?>-->
-<?php echo $table->display(); ?>
-<?php //echo $event_id;?>
-<!--<a href="admin.php?page=event-<?php echo $event_id ?>-participants&action=create"><?php _e("Add new", "evp-event") ?></a>-->
