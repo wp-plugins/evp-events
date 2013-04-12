ba9d1661bd7ec877c7c07d227e4cbc5ab7b05f14
Webtopay <Webtopay@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-12 10:56:34 +0000

    Deleted files
    git-svn-id: https://plugins.svn.wordpress.org/evp-events/trunk@696521 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tmpl/admin/discount_codes_list.php
+++
@@ -1,6 +0,0 @@
-<h2><?php _e("Discount codes", "evp-event") ?></h2>
-<?php if(isset($_GET['success'])): ?>
-	<div id="message" class="updated below-h2"><p><?php _e("Discount code successfully saved", "evp-event") ?></p></div>
-<? endif ?>
-<?php echo $table->display(); ?>
-<a href="admin.php?page=event-<?php echo $event_id ?>-discount-codes&action=create"><?php _e("Add new", "evp-event") ?></a>
--- tmpl/payment_bankwire.php
+++
@@ -1,1 +0,0 @@
-<?=  str_replace('[pdf_url]', $pdf_url, __("Payment bank wire page [pdf_url]", "evp-event")) ?>
